d6700992d941e5d1c46943fd986c3835e6907a0d
steve zhang <871506263@qq.com>
2021-02-03 14:14:58 +0800

    first commit

---
+++ 基础篇8-10/vue-demo1/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <hello-world msg="hello, world" />
+    <hr />
+    <input v-model="message" />
+    <input :value="message" @input="handleChange" />
+    <div>{{ message }} | {{ message + '-' + message }}</div>
+    <div class="bg" :id="message"></div>
+    <hr />
+    <todo-list>
+      <todo-item @delete="handleDelete" v-for="item in list" :key="item.id" :title="item.title" :del="item.del">
+        <template v-slot:pre-icon="{ icon }">
+          <span> 前置图标: {{ icon }} </span>
+        </template>
+        <template v-slot:suf-emoji="{ emoji }">
+          <span> 后置图标: {{ emoji }} </span>
+        </template>
+      </todo-item>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+//App.vue是根组件
+const TodoList = () => import('@/components/TodoList.vue');
+const TodoItem = () => import('@/components/TodoItem.vue');
+const HelloWorld = () => import('@/components/HelloWorld.vue');
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem,
+    TodoList,
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'hello world',
+      list: [
+        {
+          id: '001',
+          title: '课程1',
+          del: false
+        },
+        {
+          id: '002',
+          title: '课程2',
+          del: true
+        }
+      ]
+    };
+  },
+  methods: {
+    handleChange(e) {
+      this.message = e.target.value;
+    },
+    handleDelete(val) {
+      // eslint-disable-next-line no-console
+      console.log('handleDelete', val);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#red {
+  background: red;
+}
+#blue {
+  background: blue;
+}
+#purple {
+  background: purple;
+}
+#yellow {
+  background: yellow;
+}
+
+.bg {
+  width: 100%;
+  height: 50px;
+  background-color: #ccc;
+}
+</style>
---
+++ 基础篇8-10/vue-demo1/src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li>
+    <slot name="pre-icon" :icon="icon"> #默认前置图标# </slot>
+    <span class="red" v-if="!del">{{ title }}</span>
+    <span v-else style="text-decoration: line-through">{{ title }}</span>
+    <slot name="suf-emoji" :emoji="emoji"> *默认后置表情* </slot>
+    <button v-show="!del" @click="handleClick">删除</button>
+  </li>
+</template>
+<script>
+export default {
+  name: 'TodoItem',
+  data: function() {
+    return {
+      icon: Math.random(),
+      emoji:'😢'
+    };
+  },
+  props: {
+    title: String,
+    del: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    handleClick() {
+      // eslint-disable-next-line no-console
+      console.log('点击删除按钮');
+      this.$emit('delete', this.title);
+    }
+  }
+};
+</script>
+<style scoped>
+.red {
+  color: red;
+}
+
+button {
+  margin-left: 5px;
+}
+</style>
