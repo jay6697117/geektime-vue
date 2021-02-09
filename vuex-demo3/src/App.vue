c5058d4d1fbe99a64d5356ed74b9575506681415
steve zhang <871506263@qq.com>
2021-02-09 21:34:11 +0800

    提交代码于: 2021-02-09 21:34:11

--- vuex-demo3/src/App.vue
+++ vuex-demo3/src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <div id="app">
-    <h1>购物车示例</h1>
-    <p>账号: {{email}}</p>
-    <hr>
-    <h2>产品</h2>
-    <ProductList/>
-    <hr>
-    <ShoppingCart/>
+    <h1>{{ title }}</h1>
+    <p>账号: {{ email }}</p>
+    <hr />
+
+    <ProductList />
+    <hr />
+
+    <ShoppingCart />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import ProductList from './components/ProductList.vue'
-import ShoppingCart from './components/ShoppingCart.vue'
+// import { mapState } from 'vuex';
+import ProductList from './components/ProductList.vue';
+import ShoppingCart from './components/ShoppingCart.vue';
 
 export default {
-  computed: mapState({
-    email: state => state.userInfo.email
-  }),
+  data() {
+    return {
+      text: '购物车示例'
+    };
+  },
+  computed: {
+    email() {
+      return this.$store.state.userInfo.email;
+    },
+    title() {
+      return this.text;
+    }
+  },
+  // computed: mapState({
+  //   email: state => state.userInfo.email,
+  //   title: function() {
+  //     return this.text;
+  //   }
+  // }),
   components: { ProductList, ShoppingCart },
-}
+  mounted() {
+    window.vm = this._data;
+    console.log('App this.$store :>> ', this.$store);
+  }
+};
 </script>
--- vuex-demo3/src/components/ProductList.vue
+++ vuex-demo3/src/components/ProductList.vue
@@ -1,41 +1,38 @@
 <template>
-  <ul>
-    <li
-      v-for="product in products"
-      :key="product.id">
-      {{ product.title }} - {{ product.price }}
-      <br>
-      <button
-        :disabled="!product.inventory"
-        @click="addProductToCart(product)">
-        加入购物车
-      </button>
-    </li>
-  </ul>
+  <div>
+    <h2>产品</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - {{ product.price }}
+        <br />
+        <button :disabled="!product.inventory" @click="addProductToCart(product)">
+          加入购物车
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex';
 
 export default {
   computed: mapState({
-    products: state => state.products.all,
+    products: state => state.products.all
   }),
   // computed: {
   //   products(){
   //     return this.$store.state.products.all
   //   }
   // },
-  methods: mapActions('cart', [
-    'addProductToCart'
-  ]),
+  methods: mapActions('cart', ['addProductToCart']),
   // methods: {
   //   addProductToCart(product){
   //     this.$store.dispatch('cart/addProductToCart', product)
   //   }
   // },
-  created () {
-    this.$store.dispatch('products/getAllProducts')
+  created() {
+    this.$store.dispatch('products/getAllProducts');
   }
-}
+};
 </script>
